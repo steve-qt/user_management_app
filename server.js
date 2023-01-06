6e0c723c6aa3d67c7a109bc45f4a6c05c9748018
steven_qt <steven.luong.qt@gmail.com>
2023-01-06 15:47:04 -0800

    updating server with updating feature

--- server.js
+++ server.js
@@ -1,35 +1,52 @@
 //run the app with command: node server.js
 const express = require('express');
+const {MongoClient} = require('mongodb');
+const uri = "";
+const db_client = new MongoClient(uri);
+
 const app = express();
 const parser = require('body-parser');
+const { ObjectID } = require('bson');
 const port = 3080;
-
-
 const users = [];
 
 app.use(parser.json());
 
 app.get('/api/users', (req,res) => {
     console.log("Requesting for user list");
-    res.json(users);
+    const user_db = db_client.db('UserDB');
+    const users_collection = user_db.collection('users');
+    const users_array = users_collection.find().toArray(function(error,data){
+        res.json(data);
+    });
+});
+
+app.get('/api/user/:id', (req,res) => {
+    console.log("Geting user by id " + req.params.id);
+    const user_db = db_client.db('UserDB');
+    const users_collection = user_db.collection('users');
+    users_collection.findOne({ _id : new ObjectID(req.params.id)},function(err,data){
+        // console.log(data);
+        res.json(data);
+    });
+    
 });
 
 app.post('/api/adduser', (req,res) => {
     const user = req.body.user;
-    users.push(user);
+    //users.push(user);
+    const user_db = db_client.db('UserDB');
+    const users = user_db.collection('users');
+    users.insertOne(user);
     res.json('user added');
 });
 
-app.post('/api/deleteuser', (req,res) => {
-    const user_lastname = req.body.lastname;
-    console.log("Deleting user with lastname = " + user_lastname);
- 
-    id = users.findIndex(item => item.lastName == user_lastname);
-    console.log("deleted id = " + id);
-    if (id != -1)
-        users.splice(id,1);
-
-    res.json('user deleted');
+app.delete('/api/deleteuser/:id', (req,res) => {
+    console.log("Deleting user with _id = " + req.params.id);
+    const user_db = db_client.db('UserDB');
+    const users = user_db.collection('users');
+    users.deleteOne({ _id : new ObjectID(req.params.id)});
+    res.json("User Deleted");
 });
 
 app.get('/', (req,res) => {
@@ -39,5 +56,21 @@ app.get('/', (req,res) => {
 app.listen(port, ()=>{
     console.log('Server is listening at port ' + port);
 });
+
+app.post('/api/updateuser', (req,res) => {
+    const user = req.body.user;
+    console.log("updating user " + user._id);
+    console.log("updating user first name " + user.firstName);
+    const user_db = db_client.db('UserDB');
+    const users = user_db.collection('users');
+    users.updateOne(
+        {_id: new ObjectID(user._id)},
+        {
+        $set: {firstName: user.firstName, lastName: user.lastName, email: user.email},
+        $currentDate: { lastModified: true }
+    });
+    res.json('user updated');
+});
+
 
 
